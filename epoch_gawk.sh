51cf519d12ba90bd4dbff0caa3a724ba383c5e08
Alvin Khoo <it.consultant@hotmail.com>
2013-04-20 16:12:23 -0700

    added some header

--- epoch_gawk.sh
+++ epoch_gawk.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+# Author: Alvin Khoo
+# Date: 20 May 2013
+# Reference: http://stackoverflow.com/questions/16110252/need-to-compare-very-large-files-around-1-5gb-in-python/16112683#16112683
 /usr/bin/gawk 'BEGIN{
   OFS = ",";
   COUNT = 0; PREV_TIME=0; EMAIL=0;
